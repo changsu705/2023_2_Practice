1f9e1694a428e4a55af3ef34848f99beb0b7f7e9
2021137031 <2021137031@g.shingu.ac.kr>
2023-11-02 23:32:29 +0900

    feat:(코드써보기)
    오늘한거 EnemyGealthController및Projectille 따라쳐보기

---
+++ 2021137031/Assets/script/Scrpits/TowerSysetm/EnemyController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float speedMod = 1.0f;
+    public float timeSinceStart = 0.0f;
+    private MonsterPath thePath;       
+    private int currentPoint;     
+    private bool reachedEnd;
+
+    private bool modEnd = true;
+
+    void Start()
+    {
+        if (thePath == null)
+        {
+            thePath = FindObjectOfType<MonsterPath>();
+        }
+    }
+
+
+    
+    void Update()
+    {
+        if(modEnd == false)
+        {
+            timeSinceStart -= Time.deltaTime;
+
+            if(timeSinceStart <= 0.0f)
+            {
+                speedMod = 1.0f;
+                modEnd = true;
+            }
+        }
+
+        if (reachedEnd == false)
+        {
+            transform.LookAt(thePath.points[currentPoint])
+            transform.position = Vector3.MoveTowards(transform.position
+                , thePath.points[currentPoint].position, moveSpeed * Time.deltaTime * speedMod);
+            if(Vector3.Distance(transform.position, thePath.points[currentPoint].position) < 0.01f)
+            {
+                currentPoint += 1;
+                if(currentPoint >= thePath.points.Length)
+                {
+                    reachedEnd = true;
+                }                
+            }
+        }
+    }
+
+    public void SetMode(float value)
+    {
+        modEnd = false;
+        speedMod = value;
+        timeSinceStart = 2.0f;
+    }
+}
